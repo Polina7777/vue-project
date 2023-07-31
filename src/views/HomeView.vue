fa8bb508a1635ee83eb7518f26eee3b245d33631
Polina7777 <polina.produho98@mail.ru>
2023-07-31 21:32:01 +0200

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import Explode from '@/components/Explode.vue';
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Explode/>
   </main>
 </template>
